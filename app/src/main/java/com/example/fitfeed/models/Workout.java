8a36d359f0695238cc7107140fe8c8d51e49cd21
Owen Haiar <112823666+owenhaiar@users.noreply.github.com>
2024-10-29 18:07:26 -0400

    Basic workout saving to JSON.

--- app/src/main/java/com/example/fitfeed/models/Workout.java
+++ app/src/main/java/com/example/fitfeed/models/Workout.java
@@ -1,5 +1,9 @@
 package com.example.fitfeed.models;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +11,60 @@ import java.util.List;
  * Class to model an individual workout
  */
 public class Workout {
-    public List<Exercise> exercises = new ArrayList<>();
-}
+    private List<Exercise> exercises = new ArrayList<>();
+    private ZonedDateTime timestamp;
+    private String workoutName;
 
+    public Workout() {}
+
+    public void setTimestamp(ZonedDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void addExercise(String name, int sets, int reps, float weight) {
+        exercises.add(new Exercise(name, sets, reps, weight));
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Class to model an individual exercise
+     */
+    public static class Exercise {
+        private String name;
+        private int sets;
+        private int reps;
+        private float weight;
+
+        public Exercise() {}
+
+        public Exercise(String name, int sets, int reps, float weight) {
+            this.name = name;
+            this.sets = sets;
+            this.reps = reps;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getSets() {
+            return sets;
+        }
+
+        public int getReps() {
+            return reps;
+        }
+
+        public float getWeight() {
+            return weight;
+        }
+    }
+}
